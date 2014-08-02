b98e314aad3dd52df0b8f2f45d306c39df2dd4c9
irmakalt <kalt.irma@gmail.com>
2014-08-02 15:42:39 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -5,6 +5,11 @@ var menu = " \
       ##### Travaux \
       * [Piscine](1Piscine123.htm) \
       * [De marbre](2DeMarbre.htm) \
+      * [X.Beijing](3XBeijing.htm) \
+      * [Sans titre](4SansTitre.htm) \
+      * [Ballon d'essai](5Ballons.htm) \
+      * [Dos au mur](6DosAuMur.htm) \
+      * [Plis Plissés #3](7PlisPlisses3.htm) \
       ##### Éditions \
       * [Générations](/E1Generations.htm) \
       ##### Information \
