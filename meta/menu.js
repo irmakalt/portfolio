9e7309bc0876ea21e6043b328b8c9b9803b22517
irmakalt <kalt.irma@gmail.com>
2014-08-02 15:47:51 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -2,14 +2,17 @@ var menu = " \
     <div id=\"menu\" data-markdown> \
       # [IRMA KALT](index.htm) \
       <br/> \
-      ##### Travaux \
-      * [Piscine](1Piscine123.htm) \
-      * [De marbre](2DeMarbre.htm) \
-      * [X.Beijing](3XBeijing.htm) \
-      * [Sans titre](4SansTitre.htm) \
+      ##### TRAVAUX \
+      * [PISCINE](1Piscine123.htm) \
+      * [DE MARBRE](2DeMarbre.htm) \
+      * [X.BEIJING](3XBeijing.htm) \
+      * [SANS TITRE](4SansTitre.htm) \
       * [Ballon d'essai](5Ballons.htm) \
       * [Dos au mur](6DosAuMur.htm) \
       * [Plis Plissés #3](7PlisPlisses3.htm) \
+      * [Plis Plissés #2](8PlisPlisses2.htm) \
+      * [Still](9Still.htm) \
+      * [Rem Koolhass game](10RemKoolhassGame.htm) \
       ##### Éditions \
       * [Générations](/E1Generations.htm) \
       ##### Information \
