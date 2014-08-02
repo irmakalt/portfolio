084771ad522cedc69c0b671c11d3c5f4350a1a99
irmakalt <kalt.irma@gmail.com>
2014-08-02 14:45:09 +0200

    Update menu.js

--- meta/menu.js
+++ meta/menu.js
@@ -4,8 +4,8 @@ var menu = " \
       <br/> \
       ##### Drawings \
       * [Notes pour sculptures](kiosk.htm) \
-      ##### Prints \
-      * [Vrac](1Piscine123.htm) \
+      ##### Titrerererererer \
+      * [1Piscine123](1Piscine123.htm) \
       ##### Info \
       * [contact](/contact.htm) \
       * [home](/) \
